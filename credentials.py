d5736fd3dd5930c743a46999f95e5e93d1577955
Debasish <156603397+Debasish7ripathy@users.noreply.github.com>
2024-04-24 20:50:31 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,12 +1,12 @@
 # credentials.py
 db_credentials = {
     "host": "localhost",
-    "user": "root",
-    "password": "root",
+    "user": "username",
+    "password": "password",
     "auth_plugin": "mysql_native_password",  # If required by your MySQL server
-    "database": "PROJECT"  # Use 'database' instead of 'database_name'
+    "database": "db name"  # Use 'database' instead of 'database_name'
 }
 
 google_credentials = {
-    "GOOGLE_API_KEY": 'AIzaSyCoMmzToFD7aXBaLJSeDemHOJ7Mx7fifxE'
+    "GOOGLE_API_KEY": 'Own Genai key'
 }
